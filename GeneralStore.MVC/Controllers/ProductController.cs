c797cfd49284be30d57633529bca935da3e4d90d
Josh Shelton <joshshelton.sd@gmail.com>
2022-01-05 12:16:59 -0500

    added List functionality in productcontroller under GET: Product to allow the products to be listed in alphabetical order

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -17,8 +17,9 @@ namespace GeneralStore.MVC.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            // See below (modifying ApplicationDbContext class)
-            return View(_db.Products.ToList());
+            List<Product> productList = _db.Products.ToList();
+            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();          
+            return View(orderedList);
         }
 
         // Get: Product
