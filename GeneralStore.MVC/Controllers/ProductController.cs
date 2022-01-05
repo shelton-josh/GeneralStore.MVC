5a8e7ac046ce075efd8fc00c4bab4179837342e8
Josh Shelton <joshshelton.sd@gmail.com>
2022-01-05 11:37:52 -0500

    finished up razor additions for products and added create functionality to the product controller

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            // See below (modifying ApplicationDbContext class)
+            return View(_db.Products.ToList());
+        }
+
+        // Get: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
