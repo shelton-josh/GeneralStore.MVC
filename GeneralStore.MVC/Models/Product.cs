3fc8bc763b6c8d3d6243addc6f6d8b67e912d037
Josh Shelton <joshshelton.sd@gmail.com>
2022-01-05 09:39:25 -0500

    added product class to models folders and added some props

---
+++ GeneralStore.MVC/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int InventoryCount { get; set; }
+        public decimal Price { get; set; }
+        public bool IsFood { get; set; }
+    }
+}
